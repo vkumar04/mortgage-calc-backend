f0e98a1b48470c06165f23f52a60b2d29af0940f
Vick Kumar <Vick.Kumar44@gmail.com>
2023-09-15 08:39:51 -0400

    added error boundary to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { CalculatorForm } from "./components/calculatorForm/CalculatorForm";
 import { DataTable } from "./components/dataTable/DataTable";
 import { Navigation } from "./components/navigation/Navigation";
 import { amortizationData } from "./types";
+import ErrorBoundary from "./components/errorBoundary/ErrorBoundary";
 
 function App() {
   const [amortizationData, setAmortizationData] = useState<amortizationData[]>(
@@ -14,10 +15,14 @@ function App() {
       <Navigation title="Mortgage Calculator" />
       <div className="grid grid-cols-6 gap-6 p-2">
         <div className="col-span-1">
-          <CalculatorForm setAmortizationData={setAmortizationData} />
+          <ErrorBoundary>
+            <CalculatorForm setAmortizationData={setAmortizationData} />
+          </ErrorBoundary>
         </div>
         <div className="col-span-5">
-          <DataTable amortizationData={amortizationData} />
+          <ErrorBoundary>
+            <DataTable amortizationData={amortizationData} />
+          </ErrorBoundary>
         </div>
       </div>
     </>
